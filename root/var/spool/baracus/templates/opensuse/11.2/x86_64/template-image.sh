0b29039e50158af1ef49d76d85790c26bd8cc2ca
das_deniz <das_deniz@comcast.net>
2011-04-04 08:52:35 -0600

    remove obsolete spool/baracus/templates dir

--- root/var/spool/baracus/templates/opensuse/11.2/x86_64/template-image.sh
+++
@@ -1,8 +0,0 @@
-!/bin/sh
-# if you don't want to deal with the fstab,
-# you can also remove that file from your image
-# so the one created by autoyast is not overwritten
-# by your image extraction
-mv /mnt/etc/fstab /tmp/
-wget -O - http://10.10.0.162/kiwi_image.tgz 2>/dev/null| tar xfz - -C /mnt
-
